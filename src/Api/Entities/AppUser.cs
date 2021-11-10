7f189c566b06e2edf3a755a94c325f1edc93e10a
Andrei Antonik <antonik.av@gmail.com>
2021-11-10 21:42:18 +0800

    Add AppUser, Note and BaseEntity entities

---
+++ src/Api/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Noteapp.Api.Entities
+{
+    public class AppUser : BaseEntity
+    {
+        public string Email { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+}
---
+++ src/Api/Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Noteapp.Api.Entities
+{
+    public class Note : BaseEntity
+    {
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        [JsonIgnore]
+        public AppUser Author { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
