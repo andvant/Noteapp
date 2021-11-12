a87b3042f0cb024b46dfe867e55f4d35810bc2d4
Andrei Antonik <antonik.av@gmail.com>
2021-11-12 20:25:08 +0800

    Add test for ArchiveNoteService

---
+++ test/UnitTests/Api/ArchiveNoteServiceTests/Archive.cs
@@ -0,0 +1,84 @@
+﻿using Noteapp.Api.Data;
+using Noteapp.Api.Entities;
+using Noteapp.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Noteapp.UnitTests.Api.ArchiveNoteServiceTests
+{
+    public class Archive
+    {
+        [Fact]
+        public void ArchivesNoteGivenValidNoteIdAndUserId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note);
+
+            // Act
+            var result = service.Archive(userId: 1, noteId: 1);
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(note.Id, archivedNoteRepository.ArchivedNotes.Single().NoteId);
+        }
+
+        [Fact]
+        public void DoesNotArchiveGivenNonExistentNoteId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note);
+
+            // Act
+            var result = service.Archive(userId: 1, noteId: 2);
+
+            // Assert
+            Assert.False(result);
+            Assert.Empty(archivedNoteRepository.ArchivedNotes);
+        }
+
+        [Fact]
+        public void DoesNotArchiveGivenWrongUserId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note);
+
+            // Act
+            var result = service.Archive(userId: 2, noteId: 1);
+
+            // Assert
+            Assert.False(result);
+            Assert.Empty(archivedNoteRepository.ArchivedNotes);
+        }
+    }
+}
---
+++ test/UnitTests/Api/ArchiveNoteServiceTests/Unarchive.cs
@@ -0,0 +1,121 @@
+﻿using Noteapp.Api.Data;
+using Noteapp.Api.Entities;
+using Noteapp.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Noteapp.UnitTests.Api.ArchiveNoteServiceTests
+{
+    public class Unarchive
+    {
+        [Fact]
+        public void UnarchivesNoteGivenValidNoteIdAndUserId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note1 = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            var note2 = new Note()
+            {
+                Id = 2,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note1);
+            noteRepository.Notes.Add(note2);
+
+            var archivedNote1 = new ArchivedNote()
+            {
+                Id = 10,
+                Note = note1,
+                NoteId = note1.Id
+            };
+            var archivedNote2 = new ArchivedNote()
+            {
+                Id = 20,
+                Note = note2,
+                NoteId = note2.Id
+            };
+            archivedNoteRepository.ArchivedNotes.Add(archivedNote1);
+            archivedNoteRepository.ArchivedNotes.Add(archivedNote2);
+
+            // Act
+            var result = service.Unarchive(userId: 1, noteId: 2);
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(note1.Id, archivedNoteRepository.ArchivedNotes.Single().NoteId);
+        }
+
+        [Fact]
+        public void ReturnsFalseGivenNonExistentNoteId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note);
+
+            var archivedNote = new ArchivedNote()
+            {
+                Id = 10,
+                Note = note,
+                NoteId = note.Id
+            };
+            archivedNoteRepository.ArchivedNotes.Add(archivedNote);
+
+            // Act
+            var result = service.Unarchive(userId: 1, noteId: 2);
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(note.Id, archivedNoteRepository.ArchivedNotes.Single().NoteId);
+        }
+
+        [Fact]
+        public void ReturnsFalseGivenWrongUserId()
+        {
+            // Arrange
+            var archivedNoteRepository = new ArchivedNoteRepository();
+            var noteRepository = new NoteRepository(false);
+            var service = new ArchiveNoteService(archivedNoteRepository, noteRepository);
+
+            var note = new Note()
+            {
+                Id = 1,
+                AuthorId = 1
+            };
+            noteRepository.Notes.Add(note);
+
+            var archivedNote = new ArchivedNote()
+            {
+                Id = 10,
+                Note = note,
+                NoteId = note.Id
+            };
+            archivedNoteRepository.ArchivedNotes.Add(archivedNote);
+
+            // Act
+            var result = service.Unarchive(userId: 2, noteId: 1);
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal(note.Id, archivedNoteRepository.ArchivedNotes.Single().NoteId);
+        }
+    }
+}
