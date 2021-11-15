c5f905b86e61d0bb5192f5a793b18cfd5551a59b
Andrei Antonik <antonik.av@gmail.com>
2021-11-15 17:57:17 +0800

    Add an IExceptionFilter and apply it to NoteController to remove error handling logic from actions

--- src/Api/Controllers/NoteController.cs
+++ src/Api/Controllers/NoteController.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Noteapp.Api.Dtos;
 using Noteapp.Api.Entities;
 using Noteapp.Api.Exceptions;
+using Noteapp.Api.Filters;
 using Noteapp.Api.Services;
 using System;
 using System.Collections.Generic;
 
 namespace Noteapp.Api.Controllers
 {
+    [NoteExceptionFilter]
     [Route("api/notes")]
     [ApiController]
     public class NoteController : ControllerBase
@@ -48,173 +50,85 @@ namespace Noteapp.Api.Controllers
         [HttpGet("{id:int}")]
         public IActionResult Get(int id)
         {
-            try
-            {
-                var note = _noteService.Get(_userId, id);
-                return Ok(note);
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            var note = _noteService.Get(_userId, id);
+            return Ok(note);
         }
 
         [HttpPut("{id:int}")]
         public IActionResult Update(int id, UpdateNoteDto dto)
         {
-            try
-            {
-                _noteService.Update(_userId, id, dto.Text);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (NoteLockedException)
-            {
-                return Forbid(); // might want to change the status code
-            }
+            _noteService.Update(_userId, id, dto.Text);
+            return NoContent();
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
-            try
-            {
-                _noteService.Delete(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Delete(_userId, id);
+            return NoContent();
         }
 
         [HttpPut("{id:int}/lock")]
         public IActionResult Lock(int id)
         {
-            try
-            {
-                _noteService.Lock(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Lock(_userId, id);
+            return NoContent();
         }
 
         [HttpDelete("{id:int}/lock")]
         public IActionResult Unlock(int id)
         {
-            try
-            {
-                _noteService.Unlock(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Unlock(_userId, id);
+            return NoContent();
         }
 
         [HttpPut("{id:int}/archive")]
         public IActionResult Archive(int id)
         {
-            try
-            {
-                _noteService.Archive(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Archive(_userId, id);
+            return NoContent();
         }
 
         [HttpDelete("{id:int}/archive")]
         public IActionResult Unarchive(int id)
         {
-            try
-            {
-                _noteService.Unarchive(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Unarchive(_userId, id);
+            return NoContent();
         }
 
         [HttpPut("{id:int}/pin")]
         public IActionResult Pin(int id)
         {
-            try
-            {
-                _noteService.Pin(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Pin(_userId, id);
+            return NoContent();
         }
 
         [HttpDelete("{id:int}/pin")]
         public IActionResult Unpin(int id)
         {
-            try
-            {
-                _noteService.Unpin(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Unpin(_userId, id);
+            return NoContent();
         }
 
         [HttpPut("{id:int}/publish")]
         public IActionResult Publish(int id)
         {
-            try
-            {
-                var url = _noteService.Publish(_userId, id);
-                return CreatedAtAction(nameof(GetPublishedNoteText), new { url = url }, null);
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            var url = _noteService.Publish(_userId, id);
+            return CreatedAtAction(nameof(GetPublishedNoteText), new { url = url }, null);
         }
 
         [HttpDelete("{id:int}/publish")]
         public IActionResult Unpublish(int id)
         {
-            try
-            {
-                _noteService.Unpublish(_userId, id);
-                return NoContent();
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            _noteService.Unpublish(_userId, id);
+            return NoContent();
         }
 
         [HttpGet("/p/{url}")]
         public IActionResult GetPublishedNoteText(string url)
         {
-            try
-            {
-                var text = _noteService.GetPublishedNoteText(url);
-                return Ok(text);
-            }
-            catch (NoteNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            var text = _noteService.GetPublishedNoteText(url);
+            return Ok(text);
         }
     }
 }
---
+++ src/Api/Filters/NoteExceptionFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Noteapp.Api.Exceptions;
+using System;
+
+namespace Noteapp.Api.Filters
+{
+    public class NoteExceptionFilterAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            switch (context.Exception)
+            {
+                case NoteNotFoundException:
+                    context.Result = new NotFoundObjectResult(context.Exception.Message);
+                    break;
+                case NoteLockedException:
+                    context.Result = new ConflictObjectResult(context.Exception.Message);
+                    break;
+            }
+        }
+    }
+}
