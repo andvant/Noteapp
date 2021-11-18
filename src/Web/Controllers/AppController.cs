70119825d78c433fe27697e4860a96802e6b5175
Andrei Antonik <antonik.av@gmail.com>
2021-11-18 21:47:43 +0800

    Add MVC project (Web client of the Api) to solution

---
+++ src/Web/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
