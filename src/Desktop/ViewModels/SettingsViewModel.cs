1ace0f53c9e5c2184ef6a9692cfbdc55391090be
Andrei Antonik <antonik.av@gmail.com>
2021-12-06 17:48:01 +0800

    Desktop: bind encryption checkbox to Command in Settings view; fix some styles

--- src/Desktop/ViewModels/SettingsViewModel.cs
+++ src/Desktop/ViewModels/SettingsViewModel.cs
@@ -13,20 +13,18 @@ namespace Noteapp.Desktop.ViewModels
         private readonly ApiService _apiService;
 
         public string Email => SessionManager.GetUserInfo()?.Email ?? "Anonymous";
-        public bool EncryptionEnabled
-        {
-            get => SessionManager.GetUserInfo()?.EncryptionEnabled ?? false;
-            set => ToggleEncryption();
-        }
+        public bool EncryptionEnabled => SessionManager.GetUserInfo()?.EncryptionEnabled ?? false;
 
         public ICommand LogoutCommand { get; }
         public ICommand DeleteAccountCommand { get; }
+        public ICommand ToggleEncryptionCommand { get; }
 
         public SettingsViewModel(ApiService apiService)
         {
             _apiService = apiService;
             LogoutCommand = new RelayCommand(Logout);
             DeleteAccountCommand = new RelayCommand(DeleteAccount);
+            ToggleEncryptionCommand = new RelayCommand(ToggleEncryption);
         }
 
         private void Logout()
@@ -44,16 +42,16 @@ namespace Noteapp.Desktop.ViewModels
             LogoutCommand.Execute(null);
         }
 
-        private void ToggleEncryption()
+        private async void ToggleEncryption()
         {
             var userInfo = SessionManager.GetUserInfo();
             if (userInfo is null)
             {
-                MessageBox.Show("Have to be logged in to enable encryption.");
+                MessageBox.Show("You have to be logged in to enable encryption!");
                 return;
             }
             userInfo.EncryptionEnabled = !userInfo.EncryptionEnabled;
-            SessionManager.SaveUserInfo(userInfo).RunSynchronously();
+            await SessionManager.SaveUserInfo(userInfo);
         }
     }
 }
