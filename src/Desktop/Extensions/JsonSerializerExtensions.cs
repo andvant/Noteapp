d915caba1329c37578d99972b1ac78147183108e
Andrei Antonik <antonik.av@gmail.com>
2021-12-09 20:13:51 +0800

    Desktop: modify JsonSerializer options

--- src/Desktop/Extensions/JsonSerializerExtensions.cs
+++ src/Desktop/Extensions/JsonSerializerExtensions.cs
@@ -4,16 +4,22 @@ namespace Noteapp.Desktop.Extensions
 {
     public static class JsonSerializerExtensions
     {
-        private static JsonSerializerOptions options = new() { WriteIndented = true };
+        private readonly static JsonSerializerOptions _options = new()
+        { 
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            IgnoreReadOnlyProperties = true
+        };
 
         public static string ToJson<T>(this T value)
         {
-            return JsonSerializer.Serialize(value, options);
+            return JsonSerializer.Serialize(value, _options);
         }
 
         public static T FromJson<T>(this string json)
         {
-            return JsonSerializer.Deserialize<T>(json);
+            return JsonSerializer.Deserialize<T>(json, _options);
         }
     }
 }
